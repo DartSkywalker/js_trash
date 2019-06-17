bb6b37d45c0be4944929f0d35f30c86253978bab
Dart <Dart@Skywalker.com>
2019-06-17 15:32:24 +0300

    Callback simple

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-//Cycles
-
-let num = 50;
-
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        break;
-    }
-    console.log(i);
+function learnRS(lang, callback) {
+    console.log("I am learning " + lang);
+    callback();
 }
+
+learnRS("JS", function() {
+    console.log("CallBack test")
+});
