3cbadf57dc6384c91221799e25287f4817d8ca91
Dart <Dart@Skywalker.com>
2019-06-17 13:48:33 +0300

    test

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ console.log(a);
 
 // let answer = +prompt('enter');
 // console.log(typeof(answer));
-// //унарный плюс перед функцией (промпт) дает не стринг на выходе, а число
 
 let incr = 10,
     decr = 10;
