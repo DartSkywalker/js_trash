c27cbb135fa4cc0d9c999f8f48a2cdf35e7d9a36
Dart <Dart@Skywalker.com>
2019-06-17 13:42:31 +0300

    My first JS commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+// let answer = +prompt('enter');
+// console.log(typeof(answer));
+// //унарный плюс перед функцией (промпт) дает не стринг на выходе, а число
+
+let incr = 10,
+    decr = 10;
+
+
+
+let isChecked = true;
+let isClosed = false;
+
+console.log(isChecked === isClosed);
